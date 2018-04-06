32ce8c2e4582bdbc06a335cbe47127a923891ac4
Yousef At-tamimi <iGoLDeNZz@iGoLDeNZz-3.local>
2018-04-06 20:48:37 +0300

    hello

---
+++ Get Swifty/OTPViewController.swift
@@ -0,0 +1,88 @@
+//
+//  OTPViewController.swift
+//  Get Swifty
+//
+//  Created by Yousef At-tamimi on 4/6/18.
+//  Copyright © 2018 Yousef At-tamimi. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class OTPViewController: UIViewController {
+
+  
+    var phoneNumber : String = ""
+
+    @IBOutlet weak var codeEntered: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        sendVerification(phoneNumber: phoneNumber, code: codeEntered.text!)
+        
+    }
+    
+    func sendVerification(phoneNumber: String, code: String){
+        
+        let param : [String : String] = ["mobile":"+966\(phoneNumber)" , "code" : code]
+        Alamofire.request("https://elmhackhub.com/api/v1/auth", method: .post, parameters: param).validate().responseJSON{
+            response in
+            
+            switch response.result{
+            case .success:
+                if let JSON = response.result.value as? [String:Any]{
+                    self.storeUserInfo(json: JSON)
+                }
+                
+            case .failure(let error):
+                print(error)
+                
+                
+            }
+            
+        }
+    }
+    
+    func storeUserInfo(json : [String : Any]){
+      
+        let user = json["user"] as! [String : Any]
+        let access_token = json["access_token"] as! String
+        let user_id = user["user_id"] as! String
+        let mobile = self.phoneNumber
+        let name = user["full_name"] as! String
+        
+//        print(access_token)
+//        print(user_id)
+//        print(name)
+//        print(mobile)
+        
+        let defaults = UserDefaults.standard
+        defaults.set(access_token, forKey: String)
+        
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Get Swifty/ViewController.swift
+++ Get Swifty/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var phoneNum: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let param : [String : String] = ["mobile":"+966\(phoneNum.text!)"]
+        login(phoneNumber: param)
+    }
+    
+    
+    func login(phoneNumber : [String:String]){
+        
+        Alamofire.request("https://elmhackhub.com/api/v1/users", method: .post, parameters: phoneNumber).validate().responseJSON{
+        response in
+            
+            print("in func login")
+            switch response.result{
+                case .success:
+                   
+                    if let JSON = response.result.value as? [String:Any]{
+                        print(JSON);
+                        let res = JSON["success"]!
+                        print(res)
+                        self.performSegue(withIdentifier: "OTPView", sender: self)
+                }
+                
+                case .failure(let error):
+                    print(error)
+
+                
+            }
+          
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let OTP = segue.destination as? OTPViewController {
+            OTP.phoneNumber = phoneNum.text!
+        }
+    }
+    
 
 }
 
